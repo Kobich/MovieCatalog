b706ba46e33f8212068706689e7e7661ac592e77
alexandr <kopanov_03@mail.ru>
2025-10-04 16:27:25 +0700

    divide di by feuture

---
+++ app/src/main/java/com/moviecatalog/ui/catalog/di/CatalogModule.kt
@@ -0,0 +1,11 @@
+﻿package com.moviecatalog.ui.catalog.di
+
+import com.moviecatalog.ui.catalog.domain.CatalogInteractor
+import com.moviecatalog.ui.catalog.ui.CatalogViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val catalogModule = module {
+    single { CatalogInteractor(moviesFeature = get()) }
+    viewModel { CatalogViewModel(interactor = get()) }
+}
---
+++ app/src/main/java/com/moviecatalog/ui/details/di/DetailsModule.kt
@@ -0,0 +1,11 @@
+﻿package com.moviecatalog.ui.details.di
+
+import com.moviecatalog.ui.details.domain.DetailsInteractor
+import com.moviecatalog.ui.details.ui.DetailsViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val detailsModule = module {
+    single { DetailsInteractor(moviesFeature = get()) }
+    viewModel { (movieId: Int) -> DetailsViewModel(movieId = movieId, interactor = get()) }
+}
