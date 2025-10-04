1f1b4614e109b792af43d9567f8edb8d72d255fa
Roman Savelev <roman.savelev@gmail.com>
2025-10-04 13:03:08 +0300

    Сделал details как feature с api + impl

--- app/src/main/java/com/moviecatalog/ui/details/impl/ui/entity/MovieDetailsUiState.kt
+++ app/src/main/java/com/moviecatalog/ui/details/impl/ui/entity/MovieDetailsUiState.kt
@@ -1,11 +1,8 @@
-package com.moviecatalog.ui.details.ui.entity
+package com.moviecatalog.ui.details.impl.ui.entity
 
 import com.moviecatalog.feature.movies.api.entity.Movie
 
-
-
-
-sealed class MovieDetailsUiState {
+internal sealed class MovieDetailsUiState {
     data object Loading : MovieDetailsUiState()
     data class Content(val state: Movie) : MovieDetailsUiState()
     data class Error(val message: String) : MovieDetailsUiState()
