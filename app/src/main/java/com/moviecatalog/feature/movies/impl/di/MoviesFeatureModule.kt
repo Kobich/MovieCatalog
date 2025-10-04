5406515e18a6451aa9cf364a1a8eb3ec865b2e53
alexandr <kopanov_03@mail.ru>
2025-10-04 16:27:25 +0700

    divide di by feuture

---
+++ app/src/main/java/com/moviecatalog/feature/movies/impl/di/MoviesFeatureModule.kt
@@ -0,0 +1,14 @@
+﻿package com.moviecatalog.feature.movies.impl.di
+
+import com.moviecatalog.feature.movies.api.MoviesFeature
+import com.moviecatalog.feature.movies.impl.MoviesFeatureImpl
+import com.moviecatalog.feature.movies.impl.data.MovieRepositoryImpl
+import com.moviecatalog.feature.movies.impl.domain.MovieRepository
+import com.moviecatalog.feature.movies.impl.domain.MoviesDataInteractor
+import org.koin.dsl.module
+
+val moviesFeatureModule = module {
+    single<MovieRepository> { MovieRepositoryImpl() }
+    single { MoviesDataInteractor(repo = get()) }
+    single<MoviesFeature> { MoviesFeatureImpl(moviesDataInteractor = get()) }
+}
