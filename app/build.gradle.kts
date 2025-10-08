64cc044fd5aa09dd923624574353f59adef16620
alexandr <kopanov_03@mail.ru>
2025-10-08 11:46:16 +0700

    gradle update for network

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,21 @@
+import java.io.File
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 }
+
+val localProperties = Properties().apply {
+    val file = File(rootProject.rootDir, "local.properties")
+    if (file.exists()) {
+        load(file.inputStream())
+    }
+}
+
+val wbApiKey = localProperties.getProperty("WB_TEST_API_KEY") ?: ""
+val wbApiBaseUrl = localProperties.getProperty("WB_API_BASE_URL") ?: "https://content-api-sandbox.wildberries.ru/"
 
 android {
     namespace = "com.moviecatalog"
@@ -14,9 +27,11 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
-
+        buildConfigField("String", "WB_TEST_API_KEY", "\"$wbApiKey\"")
+        buildConfigField("String", "WB_API_BASE_URL", "\"$wbApiBaseUrl\"")
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
 
     buildTypes {
         release {
@@ -35,15 +50,25 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
+        buildConfig = true
         compose = true
     }
 }
 
 dependencies {
 
+    //DI
     implementation(libs.koin.core)
     implementation(libs.koin.android)
     implementation(libs.koin.androidx.compose)
+
+    //Net
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+    implementation("com.squareup.retrofit2:converter-moshi:3.0.0")
+    implementation("com.squareup.moshi:moshi:1.15.2")
+    implementation("com.squareup.moshi:moshi-kotlin:1.15.2")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
 
     implementation(libs.coil.compose)
     implementation(libs.androidx.core.ktx)
