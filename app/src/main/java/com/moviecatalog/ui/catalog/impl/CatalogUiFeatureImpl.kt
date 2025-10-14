a252352fa067a864802f6aece7ce614aca8b8d03
Roman Savelev <roman.savelev@gmail.com>
2025-10-14 13:37:12 +0300

    Исправил CatalogUiFeatureImpl.kt

--- app/src/main/java/com/moviecatalog/ui/catalog/impl/CatalogUiFeatureImpl.kt
+++ app/src/main/java/com/moviecatalog/ui/catalog/impl/CatalogUiFeatureImpl.kt
@@ -4,7 +4,6 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import com.moviecatalog.ui.catalog.api.CatalogUiFeature
 import com.moviecatalog.ui.catalog.impl.ui.CatalogScreen
-import com.moviecatalog.ui.details.impl.ui.DetailsScreen
 
 internal class CatalogUiFeatureImpl: CatalogUiFeature {
 
