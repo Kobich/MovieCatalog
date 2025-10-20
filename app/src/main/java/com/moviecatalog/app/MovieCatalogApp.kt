749b2f19103b05579b1e19c11044faf40dd17fdc
alexandr <kopanov_03@mail.ru>
2025-10-20 16:10:49 +0700

    add Timber for Logging

--- app/src/main/java/com/moviecatalog/app/MovieCatalogApp.kt
+++ app/src/main/java/com/moviecatalog/app/MovieCatalogApp.kt
@@ -1,16 +1,23 @@
 ﻿package com.moviecatalog.app
 
 import android.app.Application
+import com.moviecatalog.BuildConfig
 import com.moviecatalog.di.appModules
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class MovieCatalogApp : Application() {
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
         startKoin {
             androidContext(this@MovieCatalogApp)
             modules(appModules)
         }
+
     }
 }
